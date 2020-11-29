7f0f8ac9a086c44dd75360e57a3a465fb191868c
Jacob Marthaller <https://github.com/jmarthaller>
2020-11-29 10:34:56 -0500

    new ssh key added to account

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -13,6 +13,7 @@ end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
   "Write your SQL query Here"
+  "And also write them here!"
   "SELECT users.name, users.age, SUM(pledges.amount) FROM users JOIN pledges ON users.id = pledges.user_id GROUP BY users.name;"
 end
 
